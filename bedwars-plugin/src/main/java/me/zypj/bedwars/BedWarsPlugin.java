51b66a0914d19ea12c20e533422b8e9a83ea8c4d
Samuel Henrique <150966678+zyypj@users.noreply.github.com>
2025-06-08 17:52:31 -0300

    build(project): Configuração inicial do projeto

---
+++ bedwars-plugin/src/main/java/me/zypj/bedwars/BedWarsPlugin.java
@@ -0,0 +1,17 @@
+package me.zypj.bedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BedWarsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
